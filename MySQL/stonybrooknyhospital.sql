0153842728577e50d3d3c261e9c629386263f2ce
zf81 <111600840+zf81@users.noreply.github.com>
2023-12-09 01:52:51 -0500

    Add files via upload

---
+++ MySQL/stonybrooknyhospital.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE stonybrooknyhospital;
+
+USE stonybrooknyhospital;
+
+CREATE TABLE physicians (
+    physician_id INT PRIMARY KEY AUTO_INCREMENT,
+    physician_name VARCHAR(50) NOT NULL,
+    phys_specialty VARCHAR(50) DEFAULT 'Hematology Oncology'
+);
+
+CREATE TABLE patients (
+    patient_id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    date_of_birth DATE,
+    onc_physician_id INT,
+    FOREIGN KEY (onc_physician_id) REFERENCES physicians(physician_id)
+);
+
+CREATE TABLE patientlabs (
+    lab_id INT PRIMARY KEY AUTO_INCREMENT,
+    patient_id INT,
+    physician_id INT,
+    lab_name VARCHAR(50),
+    lab_date DATE,
+    lab_results TEXT,
+    FOREIGN KEY (patient_id) REFERENCES patients(patient_id),
+    FOREIGN KEY (physician_id) REFERENCES physicians(physician_id)
+);
